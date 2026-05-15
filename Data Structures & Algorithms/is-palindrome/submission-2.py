a52dcfe0083c739da9383edd1faa271ad2e4aca2
Taher <taher.yakolawala@gmail.com>
2026-05-15 12:55:45 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(c for c in s.lower() if c.isalnum())
+        backInd = -1
+        frwdInd = 0
+        frwdstr = ""
+        backstr = ""
+        for i in range (len(s)):
+            frwdstr += s[frwdInd]
+            backstr += s[backInd]
+            backInd -= 1
+            frwdInd += 1
+        print(frwdstr)
+        print(backstr)
+        if (frwdstr == backstr):
+            return True
+        return False
+
+        # s = "".join(c for c in s.lower() if c.isalnum())
+        # left, right = 0, len(s) - 1
+        # while left < right:
+        #     if s[left] != s[right]:
+        #         return False
+        #     left += 1
+        #     right -= 1
+        # return True
