b617a11b33eace10d5b868c5a8f6d6cdfa4a635e
Taher <taher.yakolawala@gmail.com>
2026-05-19 17:00:59 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s: return True
+        pairs = {')':'(',']':'[','}':'{'}
+        stk = []
+        for char in s:
+            if stk and char in pairs and pairs[char] == stk[-1]:
+                print('pop')
+                stk.pop()
+            else:    
+                print('add')
+                stk.append(char)    
+            print(stk)
+        if not stk:
+            return True
+        return False
